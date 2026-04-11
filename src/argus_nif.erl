03fc7f59823a9d6cc43b396265ff2ca034070861
John Downey <jdowney@gmail.com>
2026-04-11 04:51:53 -0500

    fix: use recommended 16-byte salt in gen_salt

--- src/argus_nif.erl
+++ src/argus_nif.erl
@@ -11,15 +11,5 @@ hash(Password, Salt, Algorithm, TimeCost, MemoryCost, Parallelism, HashLength) -
       {error, Error}
   end.
 
-gen_random_int(Min, Max) ->
-  crypto:strong_rand_bytes(4),
-  <<Int:32/integer>> = crypto:strong_rand_bytes(4),
-  Int rem (Max - Min) + Min.
-
-%% Use a min of 64 bytes rather than the default of 32
-%% for additional security.
 gen_salt() ->
-  Bytes = gen_random_int(64, 1024),
-  base64:encode(crypto:strong_rand_bytes(Bytes)).
-
-
+  base64:encode(crypto:strong_rand_bytes(16)).
